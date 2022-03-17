37ea9d1ce278834e92d83dac18865646d62bc1ed
Mailine Nguyen <nguyen.mailine@gmail.com>
2022-03-17 16:06:40 +0100

    first commit

---
+++ src/main/java/com/example/crabe/CrabeApplication.java
@@ -0,0 +1,13 @@
+package com.example.crabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrabeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrabeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crabe/CrabeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrabeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
